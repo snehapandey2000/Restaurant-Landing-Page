92f9967a9a09ca853aecd769186887b56442533b
Sneha Pandey <snehapandey2000@users.noreply.github.com>
2022-03-11 00:24:48 +0530

    updated

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -10,8 +10,17 @@ function Header() {
       <div className="app__wrapper_info">
         <SubHeading title="Chase The New Flavour" />
         <h1 className="app__header-h1">The Key To Fine Dining</h1>
+        <p className="p__opensans" style={{ margin: "1rem 0" }}>
+          Pure art on a plate that uses Scandinavia’s seasonal larder as its
+          palette
+        </p>
+        <button type="button" className="custom__button">
+          Explore Menu
+        </button>
       </div>
-      <div className="app__wrapper_img"></div>
+      <div className="app__wrapper_img">
+        <img src={images.welcome} alt="header img" />
+      </div>
     </div>
   );
 }
