4a9fa56e36d10acfa55ba60e0b28a31f769a76d6
Sneha Pandey <snehapandey2000@users.noreply.github.com>
2022-03-10 23:52:37 +0530

    updated

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Header.css";
 
-const Header = () => (
-  <div>
-    Header
-  </div>
-);
+function Header() {
+  return (
+    <div className="app__header app__wrapper section__padding" id="home">
+      <div className="app__wrapper_info">
+        <SubHeading title="Chase The New Flavour" />
+        <h1 className="app__header-h1">The Key To Fine Dining</h1>
+      </div>
+      <div className="app__wrapper_img"></div>
+    </div>
+  );
+}
 
 export default Header;
