2c52abd0963ed0d9fec2fecc1596fff6faa26545
Sneha Pandey <snehapandey2000@users.noreply.github.com>
2022-03-13 16:32:49 +0530

    updated

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,11 +1,76 @@
-import React from 'react';
+import React, { useRef } from "react";
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRightShort,
+} from "react-icons/bs";
+import { images } from "../../constants";
+import { SubHeading } from "../../components";
+import "./Gallery.css";
 
-import './Gallery.css';
+const Gallery = () => {
+  const scrollRef = useRef(null);
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+  const scroll = (direction) => {
+    const { current } = scrollRef;
+    if (direction === "left") {
+      current.scrollLeft -= 300;
+    } else {
+      current.scrollLeft += 300;
+    }
+  };
+
+  const galleryImages = [
+    images.gallery01,
+    images.gallery02,
+    images.gallery03,
+    images.gallery04,
+  ];
+
+  return (
+    <div className="app__gallery flex__center">
+      <div className="app__gallery-content">
+        <SubHeading title="Instagram" />
+        <h1 className="headtext__cormorant">Photo Gallery</h1>
+        <p className="p__opensans" style={{ color: "#AAA", marginTop: "2rem" }}>
+          Photographs create us pause and think on the times we’ve experienced
+          before. They create us, all at the same moment, nostalgic and
+          mental.Through them, we can take a closer glance at our life because
+          they push us to concentrate on a specific time.
+        </p>
+        <button type="button" className="custom__button">
+          View More
+        </button>
+      </div>
+      <div>
+        <div className="app__gallery-images">
+          <div className="app__gallery-images_container" ref={scrollRef}>
+            {galleryImages.map((image, index) => (
+              <div
+                className="app__gallery-images_card flex__center"
+                key={`gallery_image-${index + 1}`}
+              >
+                <img src={image} alt="gallery" />
+                <BsInstagram className="" />
+              </div>
+            ))}
+          </div>
+          <div className="app__gallery-images_arrow">
+            <BsArrowLeftShort
+              className="gallery__arrow-icon"
+              onClick={() => scroll("left")}
+            />
+            <BsArrowRightShort
+              className="gallery__arrow-icon"
+              onClick={() => scroll("left")}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Gallery;
+
+//2:26:44
