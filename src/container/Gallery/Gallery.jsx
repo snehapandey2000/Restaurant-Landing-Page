ad53abe3129f34269a393944e7b26fcf07b22d98
Sneha Pandey <snehapandey2000@users.noreply.github.com>
2022-03-13 23:10:39 +0530

    updated

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -28,7 +28,7 @@ const Gallery = () => {
   ];
 
   return (
-    <div className="app__gallery flex__center">
+    <div className="app__gallery">
       <div className="app__gallery-content">
         <SubHeading title="Instagram" />
         <h1 className="headtext__cormorant">Photo Gallery</h1>
@@ -42,29 +42,27 @@ const Gallery = () => {
           View More
         </button>
       </div>
-      <div>
-        <div className="app__gallery-images">
-          <div className="app__gallery-images_container" ref={scrollRef}>
-            {galleryImages.map((image, index) => (
-              <div
-                className="app__gallery-images_card flex__center"
-                key={`gallery_image-${index + 1}`}
-              >
-                <img src={image} alt="gallery" />
-                <BsInstagram className="" />
-              </div>
-            ))}
-          </div>
-          <div className="app__gallery-images_arrow">
-            <BsArrowLeftShort
-              className="gallery__arrow-icon"
-              onClick={() => scroll("left")}
-            />
-            <BsArrowRightShort
-              className="gallery__arrow-icon"
-              onClick={() => scroll("left")}
-            />
-          </div>
+      <div className="app__gallery-images">
+        <div className="app__gallery-images_container" ref={scrollRef}>
+          {galleryImages.map((image, index) => (
+            <div
+              className="app__gallery-images_card flex__center"
+              key={`gallery_image-${index + 1}`}
+            >
+              <img src={image} alt="gallery" />
+              <BsInstagram className="gallery__image-icon" />
+            </div>
+          ))}
+        </div>
+        <div className="app__gallery-images_arrows">
+          <BsArrowLeftShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("left")}
+          />
+          <BsArrowRightShort
+            className="gallery__arrow-icon"
+            onClick={() => scroll("right")}
+          />
         </div>
       </div>
     </div>
@@ -72,5 +70,3 @@ const Gallery = () => {
 };
 
 export default Gallery;
-
-//2:26:44
